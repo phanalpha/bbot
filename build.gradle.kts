06cc97935b968b3bba895b566668bd25a0904ce6
tsai phan <phanalpha@hotmail.com>
2024-06-05 22:27:39 +0800

    get account.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,12 @@
+val ktor_version: String by project
+val hoplite_version: String by project
+val clikt_version: String by project
+
 plugins {
-    kotlin("jvm") version "1.9.23"
+    kotlin("jvm") version "2.0.0"
+    kotlin("plugin.serialization") version "2.0.0"
+
+    application
 }
 
 group = "dev.alonfalsing"
@@ -10,6 +17,19 @@ repositories {
 }
 
 dependencies {
+    implementation("io.ktor:ktor-client-core:$ktor_version")
+    implementation("io.ktor:ktor-client-cio:$ktor_version")
+    implementation("io.ktor:ktor-client-content-negotiation:$ktor_version")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.0-RC")
+    implementation("org.kotlincrypto.macs:hmac-sha2:0.5.1")
+    implementation("com.sksamuel.hoplite:hoplite-core:$hoplite_version")
+    implementation("com.sksamuel.hoplite:hoplite-yaml:$hoplite_version")
+    implementation("com.github.ajalt.clikt:clikt:$clikt_version")
+
+    runtimeOnly("org.slf4j:slf4j-simple:2.1.0-alpha1")
+
     testImplementation(kotlin("test"))
 }
 
@@ -18,4 +38,8 @@ tasks.test {
 }
 kotlin {
     jvmToolchain(21)
+}
+
+application {
+    mainClass.set("dev.alonfalsing.MainKt")
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,133 @@
 package dev.alonfalsing
 
-fun main() {
-    println("Hello World!")
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.subcommands
+import com.sksamuel.hoplite.ConfigLoaderBuilder
+import com.sksamuel.hoplite.addResourceSource
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.get
+import io.ktor.client.request.headers
+import io.ktor.http.formUrlEncode
+import io.ktor.http.path
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.coroutines.runBlocking
+import kotlinx.datetime.Clock
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonContentPolymorphicSerializer
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.jsonObject
+import org.kotlincrypto.macs.hmac.sha2.HmacSHA256
+import java.math.BigDecimal
+
+data class BinanceConfiguration(
+    val apiKey: String,
+    val apiSecret: String,
+    val baseUrl: String,
+    val websocketUrl: String,
+)
+
+data class ApplicationConfiguration(
+    val binance: BinanceConfiguration,
+)
+
+@Serializable(with = AccountResponseSerializer::class)
+sealed interface AccountResponse
+
+@Serializable
+data class Error(
+    val code: Int,
+    @SerialName("msg")
+    val message: String,
+) : AccountResponse
+
+@Serializable
+data class Balance(
+    val asset: String,
+    @Serializable(with = BigDecimalSerializer::class)
+    val free: BigDecimal,
+    @Serializable(with = BigDecimalSerializer::class)
+    val locked: BigDecimal,
+)
+
+@Serializable
+data class Account(
+    val uid: Long,
+    val balances: List<Balance>,
+) : AccountResponse
+
+object AccountResponseSerializer : JsonContentPolymorphicSerializer<AccountResponse>(AccountResponse::class) {
+    override fun selectDeserializer(element: JsonElement) =
+        when {
+            "code" in element.jsonObject -> Error.serializer()
+            else -> Account.serializer()
+        }
 }
+
+class Application : CliktCommand() {
+    private lateinit var _configuration: ApplicationConfiguration
+    private lateinit var _client: HttpClient
+
+    override fun run() {
+        _configuration =
+            ConfigLoaderBuilder
+                .default()
+                .addResourceSource("/application.override.yaml", true)
+                .addResourceSource("/application.yaml")
+                .strict()
+                .build()
+                .loadConfigOrThrow<ApplicationConfiguration>()
+
+        _client =
+            HttpClient(CIO) {
+                install(ContentNegotiation) {
+                    json(
+                        Json {
+                            ignoreUnknownKeys = true
+                        },
+                    )
+                }
+            }
+
+        currentContext.obj = this
+    }
+
+    val configuration get() = _configuration
+    val client get() = _client
+}
+
+class GetAccount : CliktCommand() {
+    @OptIn(ExperimentalStdlibApi::class)
+    override fun run() =
+        runBlocking {
+            val application = currentContext.findObject<Application>()!!
+
+            application.client
+                .get(application.configuration.binance.baseUrl) {
+                    url {
+                        path("/api/v3/account")
+                        Clock.System.now().let {
+                            parameters.append("omitZeroBalances", "true")
+                            parameters.append("timestamp", it.toEpochMilliseconds().toString())
+                        }
+                        HmacSHA256(
+                            application.configuration.binance.apiSecret
+                                .toByteArray(),
+                        ).let {
+                            it.update(parameters.build().formUrlEncode().toByteArray())
+                            parameters.append("signature", it.doFinal().toHexString())
+                        }
+                    }
+                    headers {
+                        append("X-MBX-APIKEY", application.configuration.binance.apiKey)
+                    }
+                }.body<AccountResponse>()
+                .let(::println)
+        }
+}
+
+fun main(args: Array<String>) = Application().subcommands(GetAccount()).main(args)
